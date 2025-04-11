eda172916a664386d41390cf137cf33dbe1f370c
SoHanDz <aquatic0514@gmail.com>
2025-04-11 09:07:38 +0700

    Cập nhật trang tin nhắn

--- src/Components/Layout/Messages/Messenger.ts
+++ src/Components/Layout/Messages/Messenger.ts
@@ -1,6 +1,8 @@
 import { User } from "./User";
 
 export interface Messenger {
+  isOnline: any;
+  unreadCount: number;
   user: User;
   lastMessage: string;
   lastMessageTime: string;
--- src/Components/Layout/Messages/User.ts
+++ src/Components/Layout/Messages/User.ts
@@ -1,4 +1,5 @@
 export interface User {
+    isOnline: any;
     id: number;
     username: string;
     avatar?: string | null; // Cho phép avatar là null hoặc undefined
